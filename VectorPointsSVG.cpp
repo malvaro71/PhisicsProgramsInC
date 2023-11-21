c162b26594fcc8cf2e675afbef1f64cacb4437b9
malvaro71 <55465735+malvaro71@users.noreply.github.com>
2023-11-21 16:50:37 +0100

    Update VectorPointsSVG.cpp

--- VectorPointsSVG.cpp
+++ VectorPointsSVG.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main() {
     // Define vector cordiantes (x, y) and vector module and orientation as double.
-    double x1, y1, x2, y2, module, orientation;
+    double x1, y1, x2, y2, x3, y3, x4, y4, headlength, angle;
+    // Define headlength
+    headlength = 3;
     //Ask for x-coordiante of vector origin.
     cout << "Enter the x coordinate for vector origin: ";
     cin >> x1;
@@ -18,11 +20,15 @@ int main() {
     //Ask for y-coordiante of vector end. 
     cout << "Enter the y coordinate for vector end: ";
     cin >> y2;
-    //Calculate module and display vector module.
-    module = sqrt(x * x + y * y);
-    cout << "The module of the vector is: " << module << endl;
-    //Calculate and display vector orientation.
-    orientation = atan2(y, x) * 180 / M_PI;
-    cout << "The orientation of the vector is: " << orientation << " degrees" << endl;
+    //Calculate vector orientation.
+    angle = atan2(y, x) * 180 / M_PI;
+    //Calculate x and y coordinates of first partial arrowhead extreme
+    x3 = x2 + headLength * cos(angle + 220 * Math.PI / 180);
+    y3 = y2 + headLength * sin(angle + 220 * Math.PI / 180);
+    cout << "The first arrowhead extreme cordinates are (" << x3 << "," << y3 << ")";
+    //Calculate x and y coordinates of second partial arrowhead extreme
+    x4 = x2 + headLength * cos(angle + 140 * Math.PI / 180);
+    y4 = x2 + headLength * sin(angle + 140 * Math.PI / 180);
+    cout << "The second arrowhead extreme cordinates are (" << x4 << "," << y4 << ")";
     return 0;
 }
